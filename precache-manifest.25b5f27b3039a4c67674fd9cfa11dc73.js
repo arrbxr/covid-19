703dc71694aca5db1db3539f21dd4d043ba27cde
arrbxr <arr.bxr@gmail.com>
2020-04-21 10:18:09 +0530

    Updates

---
+++ precache-manifest.25b5f27b3039a4c67674fd9cfa11dc73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbf4304d7fc4bf428c1bb0e738c679a",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d6ec8d7e3b42be44e63a",
+    "url": "/covid-19/static/css/main.06e0cb57.chunk.css"
+  },
+  {
+    "revision": "cc1de371acbe725069d8",
+    "url": "/covid-19/static/js/2.1c5f2012.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.1c5f2012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ec8d7e3b42be44e63a",
+    "url": "/covid-19/static/js/main.90d32a7c.chunk.js"
+  },
+  {
+    "revision": "58ccbf4ac62906e56f32",
+    "url": "/covid-19/static/js/runtime-main.0a05f1ba.js"
+  },
+  {
+    "revision": "febe4544e4b266e372a2cd8a7d32b4e0",
+    "url": "/covid-19/static/media/ravi.febe4544.png"
+  }
+]);
